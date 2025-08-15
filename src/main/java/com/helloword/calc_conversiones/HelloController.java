9f63ff2d80291b9ccddc259f340a2d53bf0dc11e
abyslf-3 <saraaby.asl@gmail.com>
2025-08-14 21:56:36 -0600

    Se realiza la accion de los botones

--- src/main/java/com/helloword/calc_conversiones/HelloController.java
+++ src/main/java/com/helloword/calc_conversiones/HelloController.java
@@ -1,13 +1,23 @@
 package com.helloword.calc_conversiones;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
+    @FXML private TextField inputField;
+    @FXML private ComboBox<String> fromUnit;
+    @FXML private ComboBox<String> toUnit;
+    @FXML private Button convertButton;
+    @FXML private Button clearButton;
+    @FXML private Label resultLabel;
+    @FXML private ListView<String> historyList;
     @FXML
+
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
