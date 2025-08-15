a88ef7d5d7eaabc6c0c01ffdc39792455aec16d2
abyslf-3 <saraaby.asl@gmail.com>
2025-08-14 22:22:57 -0600

    editado para ejecutar.

--- src/main/java/com/helloword/calc_conversiones/HelloApplication.java
+++ src/main/java/com/helloword/calc_conversiones/HelloApplication.java
@@ -5,14 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/helloword/calc_conversiones/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("Calculadora de Conversiones");
         stage.setScene(scene);
         stage.show();
     }
